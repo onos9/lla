b5356e0ce62480e8dfd0024eda8b41b1c932eda6
Onojeta Brown <onosbrown.saved@gmail.com>
2022-01-29 20:48:34 +0000

    Update router.go

--- router.go
+++ router.go
@@ -9,6 +9,7 @@ import (
 func NewRouter() *mux.Router {
 
 	router := mux.NewRouter().StrictSlash(true)
+	.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
 	for _, route := range routes {
 		var handler http.Handler
 
