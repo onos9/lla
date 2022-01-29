eed3339faa1ebddc7166c22d8e4125ba715f8c3f
Onojeta Brown <onosbrown.saved@gmail.com>
2022-01-29 13:49:34 +0000

    Update image.go

--- image.go
+++ image.go
@@ -26,3 +26,16 @@ if err != nil {
 }
 }
 	
+func IsEmpty(name string) (bool, error) {
+    f, err := os.Open(name)
+    if err != nil {
+        return false, err
+    }
+    defer f.Close()
+
+    _, err = f.Readdirnames(1) // Or f.Readdir(1)
+    if err == io.EOF {
+        return true, nil
+    }
+    return false, err // Either not empty or error, suits both cases
+}
