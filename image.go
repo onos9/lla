4d640bf0c51b009fa2fd39a56c99bb04e9d6bf6e
Onojeta Brown <onosbrown.saved@gmail.com>
2022-01-29 13:33:09 +0000

    Edited image.go

--- image.go
+++ image.go
@@ -10,3 +10,19 @@ type Image struct {
 }
 
 type Images []Image
+
+func(img *Image)InitImageDir(){
+    err := filepath.Walk(".",
+    func(path string, info os.FileInfo, err error) error {
+    if err != nil {
+        return err
+    }
+	    
+    fmt.Println(path, info.Size())
+    return nil
+})
+if err != nil {
+    log.Println(err)
+}
+}
+	
