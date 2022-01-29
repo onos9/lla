f1d944d7bf1de4b7745679f3c568483353e956e9
Onojeta Brown <onosbrown.saved@gmail.com>
2022-01-29 14:35:49 +0000

    Update utils.js

--- web/src/helpers/utils.js
+++ web/src/helpers/utils.js
@@ -1,1 +1,10 @@
+export cost img_find = () => {
+    var imgs = document.getElementsByTagName("img");
+    var imgSrcs = [];
 
+    for (var i = 0; i < imgs.length; i++) {
+        imgSrcs.push(imgs[i].src);
+    }
+
+    return imgSrcs;
+}
