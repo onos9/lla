974c64387e8d6a30a8f6d8d050640d07bf7e83af
Onojeta Brown <onosbrown.saved@gmail.com>
2022-01-29 14:55:25 +0000

    Update main.go

--- main.go
+++ main.go
@@ -5,10 +5,56 @@ import (
 	"net/http"
 )
 
+const (
+	DefaultPort = 8080
+)
+
+var (
+	port           int64
+	rootPath       string
+	configFilePath string
+)
+
+func parseCmdLineArgs() {
+	flag.Int64Var(&port, "port", DefaultPort, "The port to listen")
+	flag.StringVar(&rootPath, "path", ".", "The path of the md files")
+	flag.StringVar(&configFilePath, "c", "conf/app.ini", "The configuration file path")
+	flag.Parse()
+}
+
 func main() {
 
 	router := NewRouter()
+	
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	parseCmdLineArgs()
+	logs.Debugf("The root path is %s", rootPath)
 
-	log.Print("Image restful API running on port :8080")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	if configFilePath != "" {
+		settings.ConfigFilePath = configFilePath
+	}
+	logs.Debugf("The configuration file path is %s", settings.ConfigFilePath)
+
+	settings.LoadAppSetting()
+
+	if port == 0 {
+		port = DefaultPort
+	}
+
+	localIps, err := utils.GetLocalIPAddrs()
+	if err != nil {
+		fmt.Println("Failed to get local ip addresses.")
+		return
+	}
+
+	fmt.Printf("Service listen on port \x1b[31;1m%d\x1b[0m and server ip addresses are \x1b[31;1m%s\x1b[0m\n", port, strings.Join(localIps, ", "))
+
+	http.Handle("/", http.StripPrefix("/", http.FileServer(helper.FSDir(rootPath))))
+
+	httpAddr := fmt.Sprintf(":%d", port)
+	if err := http.ListenAndServe(httpAddr,router) ); err != nil {
+		fmt.Printf("http.ListendAndServer() failed with %s\n", err)
+	}
+
+	fmt.Println("Exited")
 }
