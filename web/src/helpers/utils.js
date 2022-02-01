d1434c71dbff3a7accc5f2036852c6da6da418a0
Onojeta Brown <onosbrown.saved@gmail.com>
2022-02-01 22:13:20 +0100

    updated utils.js

--- web/src/helpers/utils.js
+++ web/src/helpers/utils.js
@@ -1,14 +1,40 @@
 export const InitServer = (component) => {
 
-    images = {}
+    media = {}
     const endpoint = `http://localhost:8080/images`;
     let repos = await(await fetch(endpoint)).json()
 
     if (repos === null) {
         let imgs = homeWrapper.current.getElementsByTagName("img")
-        let image_urls = Array.from(imgs).map(img => img.src)
-        images[component] = image_urls
-        console.log("Images:", images)
+        let m_urls = Array.from(imgs).map(img => img.src)
+        media[component] = image_urls
+        console.log("Images:", media)
+
+        options = {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(media)
+        }
+        const respons = await(await fetch('http://localhost:8080/images', options)).json()
+        console.log("Respons", respons)
+
+        return respons
+    }
+
+}
+
+export const initMediaUpload = (e) => {
+        e.preventDefault();
+
+        // construct the form data and apply new file name
+        var file = e.get(0).files[0];
+
+        var newFileName = file.filename + "new";
+        var formData = new FormData();
+        formData.append('file', file, newFileName);
 
         options = {
             method: 'POST',
@@ -18,35 +44,9 @@ export const InitServer = (component) => {
             },
             body: JSON.stringify(images)
         }
-        const respons = await(await fetch('http://localhost:8080/images', options)).json()
+        const respons = await(await fetch('http://localhost:8080/api/uploads/' + newFileName, options)).json()
         console.log("Respons", respons)
 
         return respons
-    }
-
 }
-export const initImageUpload = () => {
-     $("#btn-submit").click(function(e) {
-         e.preventDefault();
-          
-         // construct the form data and apply new file name
-         var file = $('#image-file').get(0).files[0];
 
-         var newFileName = file.filename + "new";
-         var formData = new FormData();
-         formData.append('file', file, newFileName);
-          
-          options = {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(images)
-          }
-          const respons = await(await fetch('http://localhost:8080/api/uploads', options)).json()
-          console.log("Respons", respons)
-
-          return respons
-       }
-  }
